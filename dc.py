e4a155a113752c851510ebf858afc029db549073
vishwajeetsuhaspatil <bhagyashreesushv2@gmail.com>
2024-11-10 20:44:57 +0530

    denomination calculation

--- dc.py
+++ dc.py
@@ -34,4 +34,34 @@ def topwin( ):
     l3= label(top, text='100', bg='light grey')
     t1= Entry(top)
     t2= Entry(top)
-    t3= Entry(top)        
+    t3= Entry(top)
+    def calculator( ):
+        try:
+            global amount
+            amount = int(entry.get())
+            note2000 = amount // 2000
+            amount %= 2000
+            note500 = amount // 500
+            amount %= 500
+            note100 = amount // 100
+            t1.delete(0, END)
+            t2.delete(0, END)
+            t3.delete(0, END)
+            t1.insert(END, str(note2000))
+            t2.insert(END, str(note500))
+            t3.insert(END, str(note100))
+        except ValueError:
+            messagebox.showerror("error", "pls enter a valid number")
+            btn = Button(top, text= 'calculate', command=calculator, bg= 'brown', fg= 'white')
+            label.place(x=230, y=50)
+            entry.place(x=200, y=50)
+            btn.place(x=240, y=120)
+            lbl.place(x=140, y=170)
+            l1.place(x=180, y=200)
+            l2.place(x=180, y=200)
+            l3.place(x=180, y=260)
+            t1.place(x=270, y=270)
+            t2.place(x=270, y=230)
+            t3.place(x=270, y=260)
+            top.mainloop( )
+root.mainloop( )
